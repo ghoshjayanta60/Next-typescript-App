775283494b1fc066f1ab5e81226fe1165a197bd9
JAYANTA <ghoshjayanta60@gmail.com>
2024-04-18 13:10:28 +0530

    new Next Typescript app

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,21 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!);
+       // mongoose.connect(process.env.MONGO_URI!);
+       mongoose.connect('mongodb+srv://jayantaghosh928:U0YTnQ8KwYN56X7E@cluster0.by6tiz0.mongodb.net/Mernn',{
+        poolSize: 5,
+        keepAlive: 1,
+        native_parser: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        reconnectInterval: 1000,
+        connectTimeoutMS: 30000,
+      //  reconnectTries: Number.MAX_VALUE,
+
+        // User Name & Password
+        user: 'jayantaghosh928',
+        pass: 'U0YTnQ8KwYN56X7E'
+    })
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
